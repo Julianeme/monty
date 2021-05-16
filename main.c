41f5abdba00f71177ce61e90f2bffa715e26bdf7
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-05-15 20:04:02 -0500

    changing swap errors

--- main.c
+++ main.c
@@ -32,7 +32,7 @@ int main(int argc, char **argv)
 	{
 		i++;
 		command = words_tokenizer(line_buf);
-		if (command[1])
+		if (command[1] && strcmp(command[0], "push") == 0)
 			global.value = value_check(command[1], i);
 		if (command[0])
 		{
